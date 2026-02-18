707574bcfe22e0d043a2bd088452026f268e690b
Jackson Levine <jack@netbug.us>
2026-02-17 20:06:37 -0800

    update

--- static/_astro/i18n.kltrk0Or.js
+++
@@ -1,1 +0,0 @@
-function n(o,a){const e=(a.BASE_URL||"/").replace(/\/$/,"");return e===""?"":e}const t={defaultLocale:"en"},s=t.defaultLocale;export{s as d,n as g};
--- static/tppc-test/_astro/siteBase.D7PHzp7q.js
+++
@@ -1,1 +0,0 @@
-const e={siteBase:"/tppc-test"};function i(t,s){return e.siteBase}export{i as g,e as i};
